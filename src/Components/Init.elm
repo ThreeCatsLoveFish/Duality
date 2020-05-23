57488700433da66a5ac5207d4a962f45580bb197
ThreeCats <121097854@qq.com>
2020-05-23 22:36:19 +0800

    Finish Playground

--- src/Components/Init.elm
+++ src/Components/Init.elm
@@ -10,11 +10,11 @@ init =
       { x = 0, y = -183, tox = 2, toy = 2, radius = 10}
   , brick =
       -- Put your bricks here!
-      [ { x = -40, y = -40, status = 1, width = 40, height = 18}
-      , { x = -100, y = 80, status = 1, width = 60, height = 30 }
-      , { x = 40, y = 40, status = 1, width = 40, height = 20}
-      , { x = 200, y = 20, status = 1, width = 50, height = 15}
-      , { x = 80, y = 40, status = 1, width = 60, height = 15}
+      [ { x = -40, y = -40, status = 1, width = 38, height = 18}
+      , { x = -20, y = 40, status = 1, width = 38, height = 18}
+      , { x = 0, y = -40, status = 1, width = 38, height = 18}
+      , { x = 20, y = 40, status = 1, width = 38, height = 18}
+      , { x = 40, y = -40, status = 1, width = 38, height = 18}
       ]
   , lock = 1
   , win = 0
