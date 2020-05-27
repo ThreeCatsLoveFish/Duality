6af45e94f6adbd2b40d9372c0afe523063517386
LighghtEeloo <2415467501@qq.com>
2020-05-27 22:48:28 +0800

    moveBall and movePaddle completed.

--- src/Bin/Update.elm
+++ src/Bin/Update.elm
@@ -8,8 +8,38 @@ update msg model =
 -- TODO
 
 
-moveBall : Model -> Model
-movePaddle : Model -> Model
+moveBall : Model -> Model -- Done
+moveBall model =
+    let
+        pos = model.ball.pos
+        v = model.ball.v
+        newPos = Point (pos.x + v.x) (pos.y + v.y)
+        setPos npos ball =
+            { ball | pos = npos }
+        setBall ball nmodel =
+            { nmodel | ball = ball}
+    in
+    setBall (setPos newPos model.ball) model
+
+movePaddle : Model -> Op -> Model -- Done
+movePaddle model op =
+    let
+        vNorm = 10 -- the speed of paddle (per 1/30 sec)
+        v = case op of
+            Left -> Point -vNorm 0
+            Right -> Point vNorm 0
+            Stay -> Point 0 0
+        pos = model.paddle.pos
+
+        newPos = Point (pos.x + v.x) (pos.y + v.y)
+        setPos npos paddle =
+            { paddle | pos = npos }
+        setPaddle paddle nmodel =
+            { nmodel | paddle = paddle}
+    in
+    setPaddle (setPos newPos model.paddle) model
+
+
 collisionCheck : Model -> Model
 gameStatus : Model -> Model
 brickStatus : Model -> Model
