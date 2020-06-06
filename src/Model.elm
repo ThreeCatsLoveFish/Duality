893be326f7be33e7b321f0a3e45faa3b79cd2348
Ehaema <65754778+Ehaema@users.noreply.github.com>
2020-06-07 01:37:50 +0800

    Complete Strangers1's visualization.

--- src/Model.elm
+++ src/Model.elm
@@ -18,6 +18,36 @@ type alias Block =
 type Color
     = Color { red : Int, green : Int, blue : Int }
 
+rgb : Int -> Int -> Int -> Color
+rgb red green blue =
+    Color { red = red, green = green, blue = blue }
+
+colorToString : Color -> String
+colorToString (Color { red, green, blue }) =
+    "rgb("
+        ++ String.fromInt red
+        ++ ","
+        ++ String.fromInt green
+        ++ ","
+        ++ String.fromInt blue
+        ++ ")"
+
+pointToString : Point -> String
+pointToString point =
+    String.fromFloat (point.x) ++ ", " ++ String.fromFloat (point.y)
+
+polyToString : Poly -> String
+polyToString poly =
+    String.join " " (List.map pointToString poly)
+
+posToPoly : number -> number -> Point -> List Point
+posToPoly w h center =
+    [ Point (center.x + w/2) (center.x + h/2)
+    , Point (center.x - w/2) (center.y + h/2)
+    , Point (center.x - w/2) (center.y - h/2)
+    , Point (center.x + w/2) (center.y - h/2)
+    ]
+
 ---
 type alias Ball =
     { pos : Point
@@ -39,8 +69,9 @@ type alias Paddle =
 type alias Brick =
     { pos: Point -- may not be necessary
     , collision: Poly -- for hitCheck
-    , block: Block
+    , block : Block
     , hitTime: HitTime
+    , color : Color
     --, visual: Visual -- can get by collision
     }
 
