b22543b0e3cb69a2bed2a7204620b746a9128b3a
LighghtEeloo <2415467501@qq.com>
2020-06-13 02:26:38 +0800

    Lovers3 completed. Never Lose (check todos). Need visualize.

--- src/Lovers3/Update.elm
+++ src/Lovers3/Update.elm
@@ -5,9 +5,11 @@ import Tools exposing (..)
 
 import CollisionBlock exposing (..)
 import CollisionPoly exposing (..)
+import Lovers3.State exposing (..)
 import Lovers3.View exposing (..)
 
 update : Msg -> Model -> ( Model, Cmd Msg )
+-- Todo: Resize
 update msg model =
     let
         model0 =
@@ -32,8 +34,8 @@ update msg model =
                     let
                         model1 = model |> getEndState
                     in
-                    { model1 | gameStatus = Animation }
-                Animation ->
+                    { model1 | gameStatus = AnimationPass }
+                AnimationPass ->
                     case msg of
                         Tick time ->
                             model |> stateIterate
@@ -158,64 +160,15 @@ winJudge model =
                 _ -> brick
         brick_all = List.map change_brick model.bricks
         ball = getBall model.ball 1
-        ball2 = getBall model.ball 2
-        closeEnough =
-            sqrt ((ball.pos.x - ball2.pos.x)^2 + (ball.pos.y - ball2.pos.y)^2) < 6.5 * ball.r
         win =
-            case closeEnough || ( brick_all |> List.filter (\b -> b.hitTime /= NoMore) |> List.isEmpty ) of
+            case ( brick_all |> List.filter (\b -> b.hitTime /= NoMore) |> List.isEmpty ) of
                 True ->
                     Pass
                 False ->
-                    case (getBall model.ball 1).pos.y > model.canvas.h+10 of
+                    case ball.pos.y > model.canvas.h+10 of
                         True -> Lose
                         False -> model.gameStatus
     in
     { model | gameStatus = win, bricks = brick_all }
 
 
-
-stateIterate : Model -> Model
-stateIterate model =
-    case List.isEmpty model.state of
-        True ->
-            { model
-            | gameStatus = ChangeLevel
-            , gameLevel = Friends2
-            }
-        _ ->
-            let
-                state = model.state
-                newState =
-                    List.map (\s -> loopState s 0.01) state
-                setModel : State -> Model -> Model
-                setModel stat model_ =
-                    case stat.name of
-                        "bezier" ->
-                            bezierBall model_ stat
-                        _ ->
-                            bezierBall model_ stat
-                newModel =
-                    List.foldl (\x y -> (setModel x y)) { model | state = newState } newState
-
-            in
-            newModel
-
-bezierBall : Model -> State -> Model
-bezierBall model state =
-    let
-        getfunc (Func func) = func
-        newBalls =
-            (getfunc state.function model state.t).ball
-    in
-    { model | ball = newBalls }
-
-getEndState : Model -> Model
-getEndState model =
-    model
-
-loopState : State -> Float -> State
-loopState state t =
-    if (state.loop == True && state.t < 1) then
-         { state | t = state.t + t}
-    else
-         { state | t = state.t - 1}
