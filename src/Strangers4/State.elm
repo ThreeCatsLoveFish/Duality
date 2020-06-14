1080ebb917848bbceb6a43a83131e26b7d92ff5b
Ehaema <65754778+Ehaema@users.noreply.github.com>
2020-06-15 01:16:08 +0800

    Standardized Strangers4

--- src/Strangers4/State.elm
+++ src/Strangers4/State.elm
@@ -1,7 +1,9 @@
 module Strangers4.State exposing (..)
 import Bezier exposing (bezierColor)
 import Fade exposing (fadeOut)
-import Model exposing (Brick, Color, HitTime(..), Model, Point, StateFunc(..), rgb)
+import Messages exposing (GameLevel(..), GameStatus(..))
+import Model exposing (Brick, Color, HitTime(..), Model, Point, State, StateFunc(..), rgb)
+import Tools exposing (dummyState)
 
 startColor = rgb 75 213 232
 endColor = rgb 208 19 72
@@ -28,6 +30,44 @@ genBezierColor p1 p2 =
     in
     bezierBrickColor
 
+stateIterate : Model -> Model
+stateIterate model =
+    case List.isEmpty model.state of
+        True ->
+            case model.gameStatus of
+                AnimationPrepare ->
+                    { model
+                    | gameStatus = Prepare
+                    }
+                AnimationPass ->
+                    { model
+                    | gameStatus = ChangeLevel
+                    , gameLevel = Companions5
+                    }
+                _ -> model
+        _ ->
+            let
+                state = model.state
+                newState =
+                    List.map (\s -> loopState s 0.007) state
+                getFunc (Func func) = func
+                setModel : State -> Model -> Model
+                setModel stat model_ =
+                    (getFunc stat.function) model_ stat.t
+                newModel =
+                    List.foldl (\x y -> (setModel x y)) { model | state = newState } newState
+
+            in
+            newModel
+
+
+getGameState : Model -> Model
+getGameState model =
+    let
+        s = dummyState
+    in
+    { model | state = [s] }
+
 getEndState : Model -> Model
 getEndState model =
     let
@@ -39,3 +79,14 @@ getEndState model =
             }
     in
     { model | state = [s1] }
+
+loopState : State -> Float -> State
+loopState state t =
+    case state.loop of
+        True ->
+            if (state.loop == True && state.t < 1) then
+                 { state | t = state.t + t}
+            else
+                 { state | t = state.t - 1}
+        False ->
+            { state | t = state.t + t}
