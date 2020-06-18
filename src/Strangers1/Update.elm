60044714e1a633339ff0cbd56519ea05229fbb36
LighghtEeloo <2415467501@qq.com>
2020-06-18 17:41:35 +0800

    Strangers1 only when closeEnough

--- src/Strangers1/Update.elm
+++ src/Strangers1/Update.elm
@@ -209,7 +209,8 @@ winJudge model =
         closeEnough =
             sqrt ((ball.pos.x - ball2.pos.x)^2 + (ball.pos.y - ball2.pos.y)^2) < 7 * ball.r
         win =
-            case closeEnough || ( brick_all |> List.filter (\b -> b.hitTime /= NoMore) |> List.isEmpty ) of
+            case closeEnough of
+            --case closeEnough || ( brick_all |> List.filter (\b -> b.hitTime /= NoMore) |> List.isEmpty ) of
                 True ->
                     Pass
                 False ->
